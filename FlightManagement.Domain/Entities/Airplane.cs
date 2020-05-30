eaca5e1cf0e1e656842682835d3c1831f62405a3
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2020-05-30 15:33:46 +0000

    Add domain classes

---
+++ FlightManagement.Domain/Entities/Airplane.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagement.Domain.Entities
+{
+    public class Airplane : BaseEntity
+    {
+        public int KeroseneConsumption { get; set; }
+        public int Speed { get; set; }
+    }
+}
