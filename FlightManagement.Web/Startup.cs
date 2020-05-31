27c4126a5a08ebdf034dd8cbed2578a6f3f78c55
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2020-05-31 22:45:51 +0100

    Update bootstrap to the latest version

--- FlightManagement.Web/Startup.cs
+++ FlightManagement.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlightManagement.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,6 +34,7 @@ namespace FlightManagement.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddInfrastructure();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
