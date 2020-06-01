dd424003815bcf7d11921f71e7b8874285d683bf
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2020-06-01 11:12:14 +0100

    Add Create operation to Airplane and Airport

---
+++ FlightManagement.Web/Controllers/AirplanesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightManagement.Domain.Entities;
+using FlightManagement.Domain.Interfaces;
+using FlightManagement.Web.ModelViews;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightManagement.Web.Controllers
+{
+    public class AirplanesController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public AirplanesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var airplanes = await _unitOfWork.AirplaneRepository.GetAllAsync();
+            return View(airplanes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AirplaneViewModel model)
+        {
+            //TODO: use automapper and remove this code from here
+            var newAirplane = new Airplane
+            {
+                Name = model.Name,
+                KeroseneConsumption = model.KeroseneConsumption,
+                Speed = model.Speed
+            };
+
+            _unitOfWork.AirplaneRepository.Add(newAirplane);
+            await _unitOfWork.SaveAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FlightManagement.Web/Controllers/AirportsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightManagement.Domain.Entities;
+using FlightManagement.Domain.Interfaces;
+using FlightManagement.Web.ModelViews;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightManagement.Web.Controllers
+{
+    public class AirportsController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public AirportsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var airports = await _unitOfWork.AirportRepository.GetAllAsync();
+            return View(airports);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AirportViewModel model)
+        {
+            //TODO: use automapper and remove this code from here
+            var newAiroport = new Airport
+            {
+                Name = model.Name,
+                Latitude = model.Latitude,
+                Longitude = model.Longitude
+            };
+            _unitOfWork.AirportRepository.Add(newAiroport);
+            await _unitOfWork.SaveAsync();
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
