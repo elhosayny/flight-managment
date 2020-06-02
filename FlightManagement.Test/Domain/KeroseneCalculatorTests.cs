444ad943e2fcab680dd1151e4f02d9172f9a38d3
Mohamed EL HOSAYNY <m.elhosayny.gh@gmail.com>
2020-06-02 10:15:42 +0100

    Refactor KeroseneCalculator Tests so it does imply testing the DistanceCalculator

--- FlightManagement.Test/Domain/KeroseneCalculatorTests.cs
+++ FlightManagement.Test/Domain/KeroseneCalculatorTests.cs
@@ -11,34 +11,19 @@ namespace FlightManagement.Test.Domain
     {
         // TODO: Refactor the KeroseneCalculator so that we can unit test it without DistanceCalculator
         [Theory]
-        [InlineData(-7.5694, 33.5849, -6.8406, 34.0048,7, 573.65)] // Casablanca -> Rabat
-        [InlineData(-5.0132, 34.0109, -7.9759, 31.6625,5, 1902.5)] // Fex -> Marrakech
-        [InlineData(-5.8328, 35.7550, -17.0880, 20.8590,10, 19859.3)] // Tanger -> Laguera
-        public void GetKeroseneQuantity_ShouldReturnCorrectQuantity(double long1, double lat1, double long2, double lat2, double keroseneConsumption,double expected)
+        [InlineData(81.95, 7, 573.65)] // Casablanca -> Rabat
+        [InlineData(380.5, 5, 1902.5)] // Fex -> Marrakech
+        [InlineData(1985.93, 10, 19859.3)] // Tanger -> Laguera
+        public void GetKeroseneQuantity_ShouldReturnCorrectQuantity(double distance, double keroseneConsumption,double expected)
         {
             var airplane = new Airplane { KeroseneConsumption = keroseneConsumption };
-            var airport1 = new Airport
-            {
-                Longitude = long1,
-                Latitude = lat1,
-            };
-
-            var airport2 = new Airport
-            {
-                Longitude = long2,
-                Latitude = lat2,
-            };
 
             var flight = new Flight
             {
-                From = airport1,
-                To = airport2,
                 Airplane = airplane
             };
 
             var keroseneCalculator = new KeroseneCalculator();
-            var distanceCalculator = new DistanceCalculator();
-            var distance = distanceCalculator.GetDistance(flight);
             var actual = keroseneCalculator.GetKeroseneQuantity(flight,distance);
 
             Assert.Equal(expected, actual);
